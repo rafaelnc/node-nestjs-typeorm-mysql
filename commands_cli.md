0fc30ca782fece6e9f89c8ca8422b86d8d2164f5
Rafael Nunes <rafaelnunes.ti@gmail.com>
2020-12-11 13:24:57 -0300

    update

--- commands_cli.md
+++ commands_cli.md
@@ -1,11 +1,9 @@
-### Inicializa Migrations ###
-npm run typeorm:migration:generate -- my_init
 
 ### Generate Migrations ###
-npm run typeorm:migration:generate --n create-users
+npm run typeorm:migration:generate --n create-product
 
 ### Create Migrations ###
-npm run typeorm:migration:create --n create-users
+npm run typeorm:migration:create --n create-product
 
 ### Execute Migrations ###
 npm run typeorm:migration:run
@@ -24,3 +22,6 @@ npm run dev
 
 ### Start Test ###
 npm run test
+
+### EXECUTE NPM IN DOCKER CONTAINER Example ####
+docker/npm run typeorm:migration:run
--- src/db/migrations/1607701506295-create-product.ts
+++ src/db/migrations/1607701506295-create-product.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class createProduct1607688440746 implements MigrationInterface {
-    name = 'createProduct1607688440746'
+export class createProduct1607701506295 implements MigrationInterface {
+    name = 'createProduct1607701506295'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("CREATE TABLE `product` (`id` int NOT NULL AUTO_INCREMENT, `name` varchar(255) NOT NULL, `description` varchar(255) NOT NULL, `price` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
