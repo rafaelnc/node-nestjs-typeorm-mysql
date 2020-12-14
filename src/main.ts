72ae2021b8ed4f4ef9933e3feddf5055dec0c34b
Rafael <rafaelcastro@avanz.com.br>
2020-12-13 22:26:00 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { configService } from './config/orm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(configService.getPort());
 }
 bootstrap();
